f79308a27dd65f492ec479d568a85779b7402fc9
gititmel <melandrea@yahoo.com>
2016-06-13 20:55:50 -0400

    added boilerplate

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php 
+ session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> MArauder's Map Boiler Plate </title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/custom.css">
+</head>
+<body>
+
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <style>
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+      #map {
+        height: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="map"></div>
+
+
+
+    
+
+
+	<script src="js/jquery.2.2.4.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/custom.js"></script>
+	<script src="obj/mapSubmit.js"></script>
+</body>
+</html>
+
+<!--search HTML5 geolocation API -->
